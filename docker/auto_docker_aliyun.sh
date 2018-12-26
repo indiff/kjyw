cbec4e3461cc0fa69a94a337ddc59b262c519d6d
indiff <indiff@126.com>
2018-12-26 19:25:21 +0800

    Update auto_docker_aliyun.sh

--- docker/auto_docker_aliyun.sh
+++ docker/auto_docker_aliyun.sh
@@ -1,50 +1,25 @@
-#!/bin/bash
-## 自动yum安装Docker，Docker配置yum源  2018-06-19
-## http://www.aqzt.com
-## email: ppabc@qq.com
-## robert yu
-## redhat 7
-
-##配置yum源
-TIME=$(date +%Y%m%d%H%M%S)
-mkdir /opt/bak-$TIME
-mv /etc/yum.repos.d/*.repo /opt/bak-$TIME/
-curl -o /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-rpm -Uvh http://mirrors.aliyun.com/epel/epel-release-latest-7.noarch.rpm
-rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
-yum-config-manager --add-repo http://mirrors.selinux.cn/help/docker-aliyun.repo
-yum makecache fast
-yum makecache
-
-##安装
-yum install -y yum-utils device-mapper-persistent-data lvm2
-yum -y install docker-ce
-chkconfig docker on
-
-##配置docker源
-tee /etc/systemd/system/docker.service <<-'EOF'
-[Unit]
-Description=Docker Application Container Engine
-Documentation=https://docs.docker.com
-After=network.target firewalld.service
-
-[Service]
-Type=notify
-ExecStart=/usr/bin/dockerd --registry-mirror=https://4ux5p520.mirror.aliyuncs.com
-ExecReload=/bin/kill -s HUP $MAINPID
-LimitNOFILE=infinity
-LimitNPROC=infinity
-LimitCORE=infinity
-TimeoutStartSec=0
-Delegate=yes
-KillMode=process
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-##启动
-service docker start
-systemctl daemon-reload
-service docker restart
-docker version
+# step 1: 安装必要的一些系统工具
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+# Step 2: 添加软件源信息
+sudo yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+# Step 3: 更新并安装Docker-CE
+sudo yum makecache fast
+sudo yum -y install docker-ce
+# Step 4: 开启Docker服务
+sudo service docker start
+# 注意：
+# 官方软件源默认启用了最新的软件，您可以通过编辑软件源的方式获取各个版本的软件包。例如官方并没有将测试版本的软件源置为可用，您可以通过以下方式开启。同理可以开启各种测试版本等。
+# vim /etc/yum.repos.d/docker-ee.repo
+#   将[docker-ce-test]下方的enabled=0修改为enabled=1
+#
+# 安装指定版本的Docker-CE:
+# Step 1: 查找Docker-CE的版本:
+# yum list docker-ce.x86_64 --showduplicates | sort -r
+#   Loading mirror speeds from cached hostfile
+#   Loaded plugins: branch, fastestmirror, langpacks
+#   docker-ce.x86_64            17.03.1.ce-1.el7.centos            docker-ce-stable
+#   docker-ce.x86_64            17.03.1.ce-1.el7.centos            @docker-ce-stable
+#   docker-ce.x86_64            17.03.0.ce-1.el7.centos            docker-ce-stable
+#   Available Packages
+# Step2: 安装指定版本的Docker-CE: (VERSION例如上面的17.03.0.ce.1-1.el7.centos)
+# sudo yum -y install docker-ce-[VERSION]
